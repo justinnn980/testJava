115a0febffdc64491ffe5c5a8275de48be90eb66
justinnn980 <justinnn980@gmail.com>
2021-07-09 17:03:34 +0900

    구구단 array

--- src/main/java/Gugudan.java
+++ src/main/java/Gugudan.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class Gugudan {
     public static void main(String[] args){
-        //2단
-        System.out.println(2 * 1);
-        System.out.println(2 * 2);
-        System.out.println(2 * 3);
-        System.out.println(2 * 4);
-        System.out.println(2 * 5);
-        System.out.println(2 * 6);
-        //commend + D -아래복사
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int[] Gogudan = new int[9];
+        for (int i=0; i<9; i++){
+            Gogudan[i] = number * (i+1);
+        }
+
+        for (int i=0; i<9; i++){
+            System.out.println(Gogudan[i]);
+        }
     }
 }
