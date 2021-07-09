4ff7df0e76f4fcf908115ad4decbe0b616b590bb
justinnn980 <justinnn980@gmail.com>
2021-07-09 15:38:18 +0900

    구구단 1

---
+++ src/main/java/Gugudan.java
@@ -0,0 +1,12 @@
+public class Gugudan {
+    public static void main(String[] args){
+        //2단
+        System.out.println(2 * 1);
+        System.out.println(2 * 2);
+        System.out.println(2 * 3);
+        System.out.println(2 * 4);
+        System.out.println(2 * 5);
+        System.out.println(2 * 6);
+        //commend + D -아래복사
+    }
+}
